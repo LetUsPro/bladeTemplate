a37ccdc18237e105104a783d7f9f46e256b4f497
duduHusky <vtmtea@gmail.com>
2021-05-07 16:51:40 +0800

    冒泡排序

--- 0x1快速排序.go
+++ 0x1快速排序.go
@@ -5,27 +5,11 @@ import (
 	"time"
 )
 
-//bubbleSort O(n^2)
-func bubbleSort(nums []int) []int {
-	if len(nums) <= 1 {
-		return nums
-	}
-	length := len(nums)
-	for i := 0; i < length; i++ {
-		for j := 0; j < length-i-1; j++ {
-			if nums[j] > nums[j+1] {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-			}
-		}
-	}
-	return nums
-}
-
 func recursionSort(arr []int) []int {
 	if len(arr) <= 1 {
 		return arr
 	}
-	pivot := arr[0]
+	pivot := arr[len(arr)/2]
 	less := make([]int, 0)
 	great := make([]int, 0)
 	equal := make([]int, 0)
@@ -45,7 +29,6 @@ func recursionSort(arr []int) []int {
 
 func main() {
 	start := time.Now()
-	fmt.Println(bubbleSort([]int{10, 45, 10, 6, 60, 31, 87, 29, 96, 6, 92, 81, 99, 72, 91, 82, 68, 33, 20, 90, 91, 67, 36, 26, 2, 70, 58, 6, 67, 91}))
 	fmt.Println(recursionSort([]int{10, 45, 10, 6, 60, 31, 87, 29, 96, 6, 92, 81, 99, 72, 91, 82, 68, 33, 20, 90, 91, 67, 36, 26, 2, 70, 58, 6, 67, 91}))
 	fmt.Println("耗时：", time.Since(start))
 }
