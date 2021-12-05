4d9dcf88d0c5871d5c16aa35b049ee232bcaefc4
duduHusky <vtmtea@gmail.com>
2021-12-05 17:56:31 +0800

    ✨ 删除排序数组中的重复项

---
+++ xx.删除排序数组中的重复项.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	mp := make(map[int]int, len(nums))
+	target := nums[:0]
+	for _, num := range nums {
+		if _, ok := mp[num]; !ok {
+			mp[num] = 1
+			target = append(target, num)
+		}
+	}
+	return len(target)
+}
+
+func main() {
+	fmt.Println(RemoveDuplicates([]int{0, 1, 2, 1, 3, 5, 2}))
+}
