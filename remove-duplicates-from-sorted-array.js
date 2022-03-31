cd29b8b14209bb6ffe484aa4d6363cffaeb182b5
duduHusky <vtmtea@gmail.com>
2022-04-01 00:06:02 +0800

    remove-duplicates-from-sorted-array

---
+++ remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+//remove-duplicates-from-sorted-array
+
+const removeDuplicates = function(nums) {
+    const n = nums.length;
+    if (n === 0) {
+        return 0;
+    }
+    let fast = 1, slow = 1;
+    while (fast < n) {
+        if (nums[fast] !== nums[fast - 1]) {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+        ++fast;
+    }
+    return slow;
+}
